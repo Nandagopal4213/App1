c09f5f16de50ac2edea62d1218a15388ea98cf85
Nandagopal4213 <menandu2324@gmail.com>
2023-03-01 15:05:05 +0530

    create coloums, image and description

---
+++ main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("D:\Python Recap\App1\images\photo.png",width=600)
+
+with col2:
+    st.title("Hari")
+    content= """
+    Hi, I'm Hari! I'm a python developer, student, and a traveller.I have worked as a freelancer for more than 3 years. I'm leaving a life that i wish to the fullest.
+    """
+    st.info(content)
