31a5191f052b4656131fa6577ae2a4e869f1f8b2
Nandagopal4213 <menandu2324@gmail.com>
2023-03-02 10:41:46 +0530

    Add description, link and image

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import pandas
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
@@ -17,3 +17,20 @@ content2 ="""
 Below you can find some of the apps I have bulit in python. Feel free to contact me!
 """
 st.write(content2)
+
+col3, empty_col,col4 = st.columns([1.5,0.5,1.5])
+
+df = pandas.read_csv("data.csv",sep=";")
+
+with col3:
+    for index,row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("D:\Python Recap\App1\images" + row["D:\Python Recap\App1\images"])
+        st.write(f"[Source Code]({row['url']})")
+with col4:
+    for index,row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("D:\Python Recap\App1\images" + row["D:\Python Recap\App1\images"])
+        st.write(f"[Source Code]({row['url']})")
